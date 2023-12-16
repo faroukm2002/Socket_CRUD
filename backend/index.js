2ce3bdfd32ed87d0ec9536ea7053852b4d0b7d8a
faroukm2002 <faroukm238@gamil.com>
2023-12-16 03:43:04 +0200

    .

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ import express from 'express'
 const app = express()
 const port = 3000
 import { Server } from 'socket.io'
-import { postModel } from './database/models/post.model.js'
+// import { postModel } from './database/models/post.model.js'
 app.get('/', (req, res) => res.send('Hello World!'))
 const server= app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
